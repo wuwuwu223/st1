d324b53fb1c5fa4f8eaaa0594ff417c3512ef958
mzfqy <admin@akile.io>
2024-03-04 11:03:14 +0800

    update

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,15 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class Triangle {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public String classify(int a, int b, int c) {
+
+        if (!((a + b > c) && (a + c > b) && (b + c > a))) {
+            return "非三角形";
+        } else if (a == b && a == c && b == c) {
+            return "等边三角形";
+        } else if (a != b && a != c && b != c) {
+            return "不等边三角形";
+        } else {
+            return "等腰三角形";
         }
     }
 }
